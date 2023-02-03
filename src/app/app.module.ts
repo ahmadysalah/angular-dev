413444b2d1e9da074950c91cf8ac7a202d51c806
ahmadysalah <a.ahmad.salah@gmail.com>
2023-02-03 22:09:31 +0200

    ng comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NameComponent } from './components/first.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, NameComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/first.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-first',
+    templateUrl: './first.component.html',
+})
+export class NameComponent implements OnInit {
+    constructor() { }
+    ngOnInit(): void { }
+}
